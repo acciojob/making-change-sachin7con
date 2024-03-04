17c782b23dff1ac628322e4e21ca22c19b69d364
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-04 17:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const makeChange = (c) => {
-  // your name here
+  //SGN your name here
+	let ob = { "q": 1, "d": 2, "n": 0, "p": 2 };
+	if(c>=25){
+		ob.q = Math.floor(c/25);
+		c= c%25;
+	}else if(c>=10 && c<25){
+		ob.d = Math.floor(c/10);
+		c= c%10;
+	}else if(c>=5 && c<10){
+		ob.n = Math.floor(c/5);
+		c= c%5;
+	}else{
+		ob.p = Math.floor(c/1);
+		c= c%1;
+	}
+
+	return ob();
 };
 
-// Do not the change the code below
+// // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
